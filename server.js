a2d0a462473bb7384ccd2da03c3225d2de5bc398
“Kevincode6” <maloukevin6@gmail.com>
2025-07-02 15:26:24 +0200

    fixed my process.env

--- server.js
+++ server.js
@@ -18,6 +18,23 @@ dotenv.config();
 // Connect to database
 connectDB();
 
+
+
+const allowedOrigins = [ process.env.FRONTEND_URL ];
+  
+  app.use(cors({
+    origin: function(origin, callback) {
+      // allow requests with no origin (like Postman, curl)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = `CORS policy does not allow access from origin: ${origin}`;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true, // enable cookies/auth headers
+  }));
+  
 const auth = require('./routes/auth');
 const tickets = require('./routes/tickets');
 const adminRoutes = require('./routes/admin');
